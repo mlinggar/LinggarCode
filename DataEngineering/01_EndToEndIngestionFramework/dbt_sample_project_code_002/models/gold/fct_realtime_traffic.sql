5197af4cddf1ff3279ba90455ac35b210a2a8b52
mlinggar <92860960+mlinggar@users.noreply.github.com>
2026-05-26 18:04:28 +0200

    Gold layer updates (#15)
    * Adding linkedService: ls_azure_function_ingestor
    
    * Adding trigger: trg_hourly_weather
    Adding pipeline: pl_ingest_nordic_traffic_data
    
    * Adding trigger: trg_hourly_trafikverket
    
    * Adding trigger: trg_weekly_openstreetmap
    
    * Updating pipeline: pl_ingest_nordic_traffic_data
    
    * Updating trigger: trg_hourly_trafikverket
    
    * Updating trigger: trg_hourly_trafikverket
    
    * Updating trigger: trg_hourly_weather
    
    * Updating trigger: trg_hourly_trafikverket
    
    * Updating trigger: trg_weekly_openstreetmap
    
    * add dbt query
    
    * Merge pull request #1 from mlinggar/gold-layer-updates
    
    add dbt query
    
    * Delete DataEngineering/01_EndToEndIngestionFramework/src directory
    
    * add virtual addition for map
    
    * Gold layer updates (#2)
    
    * add dbt query
    
    * add virtual addition for map
    
    ---------
    
    Co-authored-by: linggar.pangestu@hyperisland.se <linggar.pangestu@hyperisland.se>
    
    * update the brige route id to varchar
    
    * change route id on bridge to string
    
    * Gold layer updates (#3)
    
    * add dbt query
    
    * add virtual addition for map
    
    * update the brige route id to varchar
    
    * change route id on bridge to string
    
    ---------
    
    Co-authored-by: linggar.pangestu@hyperisland.se <linggar.pangestu@hyperisland.se>
    
    * change node id to string
    
    * Gold layer updates (#4)
    
    * add dbt query
    
    * add virtual addition for map
    
    * update the brige route id to varchar
    
    * change route id on bridge to string
    
    * change node id to string
    
    ---------
    
    Co-authored-by: linggar.pangestu@hyperisland.se <linggar.pangestu@hyperisland.se>
    
    * no null
    
    * Gold layer updates (#5)
    
    * add dbt query
    
    * add virtual addition for map
    
    * update the brige route id to varchar
    
    * change route id on bridge to string
    
    * change node id to string
    
    * no null
    
    ---------
    
    Co-authored-by: linggar.pangestu@hyperisland.se <linggar.pangestu@hyperisland.se>
    
    * change the seed
    
    * Gold layer updates (#6)
    
    * add dbt query
    
    * add virtual addition for map
    
    * update the brige route id to varchar
    
    * change route id on bridge to string
    
    * change node id to string
    
    * no null
    
    * change the seed
    
    ---------
    
    Co-authored-by: linggar.pangestu@hyperisland.se <linggar.pangestu@hyperisland.se>
    
    * add table for live visualisation
    
    * Gold layer updates (#7)
    
    * add dbt query
    
    * add virtual addition for map
    
    * update the brige route id to varchar
    
    * change route id on bridge to string
    
    * change node id to string
    
    * no null
    
    * change the seed
    
    * add table for live visualisation
    
    ---------
    
    Co-authored-by: linggar.pangestu@hyperisland.se <linggar.pangestu@hyperisland.se>
    
    * Updating trigger: trg_hourly_trafikverket
    
    * Updating trigger: trg_hourly_trafikverket
    
    * Updating trigger: trg_hourly_weather
    
    * Updating trigger: trg_hourly_trafikverket
    
    * Updating trigger: trg_hourly_trafikverket
    
    * Updating trigger: trg_hourly_weather
    
    * Gold layer updates (#8)
    
    * add dbt query
    
    * add virtual addition for map
    
    * update the brige route id to varchar
    
    * change route id on bridge to string
    
    * change node id to string
    
    * no null
    
    * change the seed
    
    * add table for live visualisation
    
    ---------
    
    Co-authored-by: linggar.pangestu@hyperisland.se <linggar.pangestu@hyperisland.se>
    
    * new table for all
    
    * Gold layer updates (#9)
    
    * add dbt query
    
    * add virtual addition for map
    
    * update the brige route id to varchar
    
    * change route id on bridge to string
    
    * change node id to string
    
    * no null
    
    * change the seed
    
    * add table for live visualisation
    
    * new table for all
    
    ---------
    
    Co-authored-by: linggar.pangestu@hyperisland.se <linggar.pangestu@hyperisland.se>
    
    * new table for everything
    
    * another change
    
    * another change
    
    * another
    
    * change table to view
    
    * add config tags
    
    ---------
    
    Co-authored-by: linggar.pangestu@hyperisland.se <linggar.pangestu@hyperisland.se>

--- DataEngineering/01_EndToEndIngestionFramework/dbt_sample_project_code_002/models/gold/fct_realtime_traffic.sql
+++ DataEngineering/01_EndToEndIngestionFramework/dbt_sample_project_code_002/models/gold/fct_realtime_traffic.sql
@@ -1,42 +1,46 @@
-{{ config(materialized='view') }}
+{{ config(
+    materialized='view',
+    tags=['road']
+) }}
 
-SELECT 
-    -- 1. PRIMARY KEY (Unique hash identifier for this fact record)
-    MD5(CONCAT(UPPER(TRIM(route_id)), '_', CAST(measure_time AS VARCHAR))) AS traffic_fact_key,
+select 
+    -- 1. PRIMARY KEY
+    md5(concat(upper(trim(route_id)), '_', to_varchar(measure_time, 'YYYYMMDDHH24MISS'))) as traffic_fact_key,
 
-    -- 2. FOREIGN KEYS (To link out to your dimension tables)
-    MD5(UPPER(TRIM(route_id))) AS route_key,
+    -- 2. FOREIGN KEYS
+    md5(upper(trim(route_id))) as route_key,
     
-    MD5(UPPER(TRIM(
-        CASE 
-            WHEN LOWER(route_name) LIKE '%göteborg%' OR LOWER(route_name) LIKE '%gothenburg%' THEN 'GOTHENBURG'
-            WHEN LOWER(route_name) LIKE '%skåne%' OR LOWER(route_name) LIKE '%malmö%' THEN 'MALMO'
-            ELSE 'STOCKHOLM'
-        END
-    ))) AS city_key,
+    md5(upper(trim(
+        case 
+            when lower(route_name) like '%göteborg%' or lower(route_name) like '%gothenburg%' then 'GOTHENBURG'
+            when lower(route_name) like '%skåne%' or lower(route_name) like '%malmö%' then 'MALMO'
+            else 'STOCKHOLM'
+        end
+    ))) as city_key,
     
-    -- 3. DEGENERATE DIMENSIONS (Natural text strings kept for easy reference)
+    -- 3. DEGENERATE DIMENSIONS
     route_id,
-    CASE 
-        WHEN LOWER(route_name) LIKE '%göteborg%' OR LOWER(route_name) LIKE '%gothenburg%' THEN 'GOTHENBURG'
-        WHEN LOWER(route_name) LIKE '%skåne%' OR LOWER(route_name) LIKE '%malmö%' THEN 'MALMO'
-        ELSE 'STOCKHOLM'
-    END AS city_name,
+    case 
+        when lower(route_name) like '%göteborg%' or lower(route_name) like '%gothenburg%' then 'GOTHENBURG'
+        when lower(route_name) like '%skåne%' or lower(route_name) like '%malmö%' then 'MALMO'
+        else 'STOCKHOLM'
+    end as city_name,
     
     -- 4. FACT METRICS
-    speed AS live_speed_kmh,
-    travel_time AS travel_time_seconds,
+    speed as live_speed_kmh,
+    travel_time as travel_time_seconds,
     traffic_status,
     
-    CASE 
-        WHEN LOWER(traffic_status) LIKE '%free%' OR LOWER(traffic_status) LIKE '%normal%' THEN 'Green'
-        WHEN LOWER(traffic_status) LIKE '%heavy%' OR LOWER(traffic_status) LIKE '%slow%' THEN 'Yellow'
-        WHEN LOWER(traffic_status) LIKE '%congested%' OR LOWER(traffic_status) LIKE '%blocked%' THEN 'Red'
-        ELSE 'Gray'
-    END AS status_color,
+    case 
+        when lower(traffic_status) like '%free%' or lower(traffic_status) like '%normal%' then 'Green'
+        when lower(traffic_status) like '%heavy%' or lower(traffic_status) like '%slow%' then 'Yellow'
+        when lower(traffic_status) like '%congested%' or lower(traffic_status) like '%blocked%' then 'Red'
+        else 'Gray'
+    end as status_color,
     
     -- 5. TIMESTAMPS
-    TO_VARCHAR(measure_time, 'YYYY-MM-DD HH24:MI:SS') AS metrics_measured_at,
-    load_timestamp AS dbt_loaded_at
+    measure_time,
+    to_varchar(measure_time, 'YYYY-MM-DD HH24:MI:SS') as metrics_measured_at,
+    load_timestamp as dbt_loaded_at
 
-FROM {{ source('trafikverket', 'silver_trafikverket') }}
+from {{ source('trafikverket', 'silver_trafikverket') }}

9d7f752474638f295fcb1c5ef86e72044e796c47
Linggar Pangesti <linggar.pangestu@hyperisland.se>
2026-05-26 17:28:20 +0000

    time change

--- DataEngineering/01_EndToEndIngestionFramework/dbt_sample_project_code_002/models/gold/dim_weather.sql
+++ DataEngineering/01_EndToEndIngestionFramework/dbt_sample_project_code_002/models/gold/dim_weather.sql
@@ -1,4 +1,15 @@
-{{ config(materialized='view', tags=['weather']) }}
+{{ config(
+    materialized='view',
+    tags=['weather']
+) }}
+
+with converted_weather as (
+    select
+        *,
+        -- Convert the weather measurement time to Swedish Time zone upfront
+        convert_timezone('Europe/Stockholm', measure_timestamp)::timestamp_ntz as measure_timestamp_se
+    from {{ source('openweather', 'silver_openweather') }}
+)
 
 select 
     md5(upper(trim(city_name))) as city_key,
@@ -8,6 +19,7 @@ select
     weather_main as weather_condition,
     weather_description,
     wind_speed,
-    measure_timestamp as weather_measured_at
-from {{ source('openweather', 'silver_openweather') }}
-qualify row_number() over (partition by city_name order by measure_timestamp desc) = 1
+    measure_timestamp_se as weather_measured_at
+from converted_weather
+-- Ensures we pick the absolute latest Swedish-timestamped record per city
+qualify row_number() over (partition by city_name order by measure_timestamp_se desc) = 1

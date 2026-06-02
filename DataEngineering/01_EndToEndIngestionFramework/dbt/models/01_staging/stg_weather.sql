fd5598a05c8e798ab7ee539132f9d6559eddd4ad
mlinggar <92860960+mlinggar@users.noreply.github.com>
2026-06-02 22:45:41 +0200

    Mlinggar patch 2 (#26)
    * dbt models for snowflake
    
    * change the view
    
    * stg weather change
    
    ---------
    
    Co-authored-by: Linggar Pangesti <linggar.pangestu@hyperisland.se>

--- DataEngineering/01_EndToEndIngestionFramework/dbt/models/01_staging/stg_weather.sql
+++ DataEngineering/01_EndToEndIngestionFramework/dbt/models/01_staging/stg_weather.sql
@@ -2,7 +2,8 @@
 
 select
     json_data:dt::int as dt_unix,
-    to_timestamp(json_data:dt::int) as observation_timestamp,
+    -- FIX: Convert UTC Unix time to Stockholm local time
+    convert_timezone('UTC', 'Europe/Stockholm', to_timestamp(json_data:dt::int)) as observation_timestamp,
     json_data:weather[0].main::string as weather_condition,
     json_data:weather[0].description::string as weather_description,
     json_data:main.temp::float as temperature_celsius,
